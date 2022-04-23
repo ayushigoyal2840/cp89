d894f7d125d13c1358b774683f650e128f535453
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-04-23 23:03:47 +0530

    Time: 226 ms (25.14%), Space: 48.1 MB (64.84%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
-    int cc=0;
-    void dfs(vector<int> adj[],int node,vector<bool>& vis)
-    {
-        vis[node]=true;
-        for(auto it:adj[node])
-        {
-            if(vis[it]==false)
-            {
-                dfs(adj,it,vis);
-            }
-        }
-    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int vertices=connections.size();
-        if(n>(vertices+1))
+        int edges=connections.size();
+        if(edges<n-1)
+        {
             return -1;
+        }
         vector<int>adj[100001];
-        vector<bool>vis(vertices,false);
-        
-        for(int i=0;i<vertices;i++)
+        vector<bool>vis(100001,false);
+        for(int i=0;i<connections.size();i++)
         {
             adj[connections[i][0]].push_back(connections[i][1]);
             adj[connections[i][1]].push_back(connections[i][0]);
         }
-        for(int i=0;i<vertices;i++)
-        {
-            cout<<i<<" ";
-            for(auto it : adj[i])
-            {
-                cout<<it<<" ";
-            }
-            cout<<endl;
-        }
+        int cc=0;
+        queue<int>q;
+        // q.push(0);
         for(int i=0;i<n;i++)
         {
-            if(vis[i]==false)
+            
+            if(!vis[i])
             {
-                dfs(adj,i,vis);
-                cc++;
+                    cc++;
+                    vis[i]=true;
+                    q.push(i);
+                while(!q.empty())
+                {
+                    int temp=q.front();
+                    q.pop();
+                    for(auto it:adj[temp])
+                    {
+                        if(!vis[it])
+                        {
+                            vis[it]=true;
+                            q.push(it);
+                        }
+                    }
+                    // q.pop();
+                }
             }
-            // dfs(adj,i);/
         }
-        cout<<"connected components are"<<cc<<endl;
-        cout<<"vertices are"<<vertices<<endl;
-        int need=cc-1;
-        int actual=n-1;
-        if(cc==1) return 0;
         return cc-1;
-        // return 0;
     }
 };
