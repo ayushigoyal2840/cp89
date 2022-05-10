d288cafebb2ef7142cbdbbd707d191b53785fed9
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-05-10 18:55:20 +0530

    Time: 7 ms (81.58%), Space: 10.8 MB (88.94%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& ans,vector<int>& ds, vector<int>& can,int target ,int start)
+    {
+        if(target<0)
+            return ;
+        if(target==0)
+        {
+            ans.push_back(ds);
+            // return ;
+        }
+        for(int i=start;i<can.size();i++)
+        {
+            ds.push_back(can[i]);
+            solve(ans,ds,can,target-can[i],i);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& can, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(ans,ds,can,target,0);
+        return ans;
+    }
+};
