bd1f548c8310a7065d5298525aedf58d066562cd
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-05-17 17:51:13 +0530

    Added solution - LeetHub

---
+++ Minimum Number in a sorted rotated array - GFG/minimum-number-in-a-sorted-rotated-array.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the minimum element in sorted and rotated array.
+    int minNumber(int arr[], int low, int high)
+    {
+        int mini=INT_MAX;
+    for(int i=low;i<=high;i++)
+    {
+        // cout<<arr[i]<<" ";
+        if(arr[i]<mini)
+        {
+            mini=arr[i];
+        }
+    }
+    // cout<<low<<" "<<high<<endl;
+    return mini;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];	
+		Solution obj;
+		cout << obj.minNumber(a,0,n-1) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
