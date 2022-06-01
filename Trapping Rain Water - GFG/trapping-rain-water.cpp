fcfb4d7fae907175a2175cb8815f50b565a541c8
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-06-01 22:37:58 +0530

    Added solution - LeetHub

---
+++ Trapping Rain Water - GFG/trapping-rain-water.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        long long ans=0;
+        int pre[n],suf[n];
+        pre[0]=arr[0];
+        suf[n-1]=arr[n-1];
+        for(int i=1;i<n;i++)
+        {
+            pre[i]=max(pre[i-1],arr[i]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            suf[i]=max(suf[i+1],arr[i]);
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            ans+=max(min(pre[i-1]-arr[i],suf[i+1]-arr[i]),0);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
