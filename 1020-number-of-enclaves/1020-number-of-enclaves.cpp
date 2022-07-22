569d7fa2ae3b8f77215059bee3e848716da594c3
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-07-22 22:31:18 +0530

    Time: 385 ms (5.06%), Space: 22.1 MB (29.85%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int tot=0,count=0;
+    void dfs(int i,int j,int n,int m ,vector<vector<int>>& grid, vector<vector<bool>>&vis)
+    {
+        
+        if(i<0 or j<0 or i>=n or j>=m or grid[i][j]==0 or vis[i][j]==true)
+            return ;
+        vis[i][j]=true;
+        tot+=1;
+        cout<<"hi"<<'\n';
+        dfs(i,j+1,n,m,grid,vis);
+        dfs(i,j-1,n,m,grid,vis);
+        dfs(i-1,j,n,m,grid,vis);
+        dfs(i+1,j,n,m,grid,vis);
+        
+    }
+     void dfss(int i,int j,int n,int m ,vector<vector<int>>& grid, vector<vector<bool>>&vis)
+    {
+        
+        if(i<0 or j<0 or i>=n or j>=m or grid[i][j]==0 or vis[i][j]==true)
+            return ;
+        vis[i][j]=true;
+        count+=1;
+        cout<<"hi"<<'\n';
+        dfss(i,j+1,n,m,grid,vis);
+        dfss(i,j-1,n,m,grid,vis);
+        dfss(i-1,j,n,m,grid,vis);
+        dfss(i+1,j,n,m,grid,vis);
+        
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        // int tot=0,count=0;
+        // vector<vector<int>>adj;
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                vis[i][j]=false;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1 and vis[i][j]==false)
+                {
+                    dfs(i,j,n,m,grid,vis);
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                vis[i][j]=false;
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1 and vis[i][j]==false and (i==0 or j==0 or i==n-1 or j==m-1))
+                {
+                    dfss(i,j,n,m,grid,vis);
+                }
+            }
+        }
+        return tot-count;
+    }
+};
