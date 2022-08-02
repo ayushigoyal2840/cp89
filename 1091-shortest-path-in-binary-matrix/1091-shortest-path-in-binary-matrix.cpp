15ab594767858edf38378be67310ca261da8487d
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-08-02 13:19:16 +0530

    Time: 173 ms (32.47%), Space: 36.9 MB (27.03%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
+     int dir [8][2] ={
+        {0,1}, // right
+        {0,-1}, //left
+        {1,0}, // down
+        {-1,0}, // up
+        {1,1},// diagonal bottom right
+        {1,-1}, // diagonal up right
+        {-1,1}, // diag up left
+        {-1,-1} // diag up right
+        };
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size(), n=grid[0].size();
-        if(grid[0][0]==1 or grid[m-1][n-1]==1) return -1;
-        queue<pair<int,int>> q;
-        vector<pair<int,int>> dir{{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-        q.push({0,0});
+        int n=grid.size();
+       
+        
+        
+        if(grid[0][0]==1 or grid[n-1][n-1]==1) return -1;
+        
+        queue<vector<int>> q;
+        q.push({0,0,1});
         grid[0][0]=1;
-        while(!q.empty()){
-            auto cur = q.front();
-            q.pop();
-            int curx = cur.first, cury = cur.second;
-            if(curx == m-1 and cury == n-1) return grid[curx][cury];
-            for(auto i: dir){
-                int x = curx + i.first;
-                int y = cury + i.second;
-                if(x>=0 and x<m and y>=0 and y<n and grid[x][y]==0){\
-                    q.push({x,y});
-                    grid[x][y] = grid[curx][cury] + 1; 
+        
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size>0)
+            {
+                vector<int> cell = q.front();
+                q.pop();
+                
+                if(cell[0]==n-1 and cell[1]==n-1) return cell[2];
+                
+                for(auto d : dir)
+                {
+                    int r = cell[0] + d[0];
+                    int c = cell[1] + d[1];
+                    
+                    if(r>=0 and c>=0 and r<n and c<n and grid[r][c]==0)
+                    {
+                        q.push({r,c,cell[2]+1});
+                        grid[r][c]=1;
+                    }
                 }
+                size--;
             }
         }
         return -1;
