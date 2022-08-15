65439d2d6cec8b60053c25985a4086dc2e1bfa10
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-08-15 22:33:10 +0530

    Time: 652 ms (10.75%), Space: 40.2 MB (89.98%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,23 +1,48 @@
 class MyHashSet {
 public:
-    map<int,int>mp;
+   struct ListNode{
+       int val;
+       ListNode* next;
+       ListNode() : val(0),next(NULL) {}
+       ListNode(int x) : val(x),next(NULL) {}
+       ListNode(int x,ListNode* next) : val(x),next(next) {}
+   };
+    ListNode* head;
     MyHashSet() {
-        
+        head=new ListNode(-1);
     }
     
     void add(int key) {
-        if(mp[key]==0)
-        mp[key]++;
+        ListNode* temp=new ListNode(key);
+        ListNode* curr=head;
+        while(curr!=NULL and curr->next)
+        {
+          if(curr->next->val == key) return;
+            curr=curr->next;
+        }
+        curr->next=temp;
     }
     
     void remove(int key) {
-        if(mp[key]>0)
-        mp[key]--;
+       ListNode* curr=head;
+        while(curr and curr->next)
+        {
+            if(curr->next->val==key)
+            {
+                curr->next=curr->next->next;
+            }
+            curr=curr->next;
+        }
     }
     
     bool contains(int key) {
-        if(mp[key]>0)
-            return true;
+        ListNode* curr=head;
+        while(curr and curr->next)
+        {
+            if(curr->next->val==key) 
+                return true;
+            curr=curr->next;
+        }
         return false;
     }
 };
