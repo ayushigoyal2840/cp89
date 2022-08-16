f0a775a2a29e6c2eb3787d495a02ac8d68c4e2f0
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-08-16 17:17:17 +0530

    Time: 117 ms (70.85%), Space: 39.4 MB (89.06%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -12,25 +12,28 @@
 class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        stack<TreeNode*> s;
+       stack<TreeNode*> s;
         TreeNode* root=NULL;
-        for(int i=0;i<nums.size();i++)
-        {
-            TreeNode* curr=new TreeNode(nums[i]);
-            while(!s.empty() and s.top()->val<curr->val)
-            {
-                curr->left=s.top();
-                s.pop();
-            }
-            if(!s.empty())
-            {
-                s.top()->right=curr;
-            }
-            else
-            {
-                root=curr;
-            }
-            s.push(curr);
+      for(int i=0;i<nums.size();i++)
+      {
+          TreeNode* curr=new TreeNode(nums[i]);
+          
+                    while(!s.empty() and (s.top()->val < curr->val))
+                  {
+                     curr->left = s.top();
+                      s.pop();
+                  }
+          
+           if(!s.empty())
+          {
+              s.top()->right = curr;
+          }
+          else
+          {
+              root=curr;
+          }
+          s.push(curr);
+              
         }
         return root;
     }
