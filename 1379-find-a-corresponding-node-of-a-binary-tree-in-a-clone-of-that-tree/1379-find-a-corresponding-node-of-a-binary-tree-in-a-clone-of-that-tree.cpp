789995d4c5a2bff4a21e2d81e8f80bda2d64aab2
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-08-19 23:50:13 +0530

    Time: 1168 ms (5.01%), Space: 163.8 MB (96.72%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -13,7 +13,7 @@ public:
     TreeNode* ans;
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
         if(cloned==NULL)
-            return cloned;
+            return NULL;
         if(cloned->val==target->val)
             ans=cloned;
         getTargetCopy(original,cloned->left,target);
