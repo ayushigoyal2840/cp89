f495ee1edb6fb85d0f9c8a019625622eabf6879a
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-08-27 21:29:10 +0530

    Time: 3 ms (97.20%), Space: 6 MB (88.40%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void backtrack(string tiles, int level, int &count)
+    void backtrack(string tiles,int level, int &count)
     {
         count++;
         for(int i=level;i<tiles.size();i++)
         {
-            if(level!=i and tiles[i]==tiles[level])
-            {
+            if(i!=level and tiles[i]==tiles[level])
                 continue;
-            }
             swap(tiles[i],tiles[level]);
-            backtrack(tiles,level+1,count);
+        backtrack(tiles,level+1,count);
         }
         
     }
     int numTilePossibilities(string tiles) {
-        int count=-1;sort(tiles.begin(),tiles.end());
+        int count=-1;
+        sort(tiles.begin(),tiles.end());
         backtrack(tiles,0,count);
         return count;
     }
