ca0b4ab8d004f7fa3e5e46694cb064c2e2b5f5bd
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-08-29 18:10:40 +0530

    Time: 323 ms (37.10%), Space: 11.3 MB (57.13%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+    
+        int ans=1;
+        int start=0;
+        
         int n=s.size();
         int dp[n][n];
         memset(dp,0,sizeof(dp));
         for(int i=0;i<s.size();i++)
         {
             dp[i][i]=1;
+           
         }
-        int start=0;
-        int ans=1;
         for(int i=0;i<s.size()-1;i++)
         {
             if(s[i]==s[i+1])
             {
-                 dp[i][i+1]=1;
-            start=i;
-            ans=2;
+                dp[i][i+1]=1;
+                ans=2;
+                start=i;
             }
-           
         }
-        for(int k=3;k<=n;k++)
+        for(int k=3;k<=s.size();k++)
         {
             for(int i=0;i<n-k+1;i++)
             {
@@ -29,16 +31,16 @@ public:
                 {
                     dp[i][j]=1;
                     if(k>ans)
-                    {
-                        ans=k;
-                        start=i;
-                    }
+                    {ans=k;
+                    start=i;}
                 }
             }
+            
         }
         string fina="";
         for(int i=start;i<start+ans;i++)
             fina+=s[i];
         return fina;
+        
     }
 };
