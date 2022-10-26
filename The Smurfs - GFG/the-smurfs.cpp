3a8912a02bc32ddedb4f7c8bb5323b6ce191ebd5
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-10-26 16:21:01 +0530

    Added solution - LeetHub

---
+++ The Smurfs - GFG/the-smurfs.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findMin(int n, char a[]){
+       map<char,int> m;
+for(int i=0;i<n;i++)  m[a[i]]++;
+if(m['R']%2 == m['G']%2 && m['G']%2 == m['B']%2) return 2;
+else return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        char a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        cout<<ob.findMin(n, a)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
