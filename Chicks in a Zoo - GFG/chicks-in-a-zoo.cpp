36beb6cc7d93b33de8d89ac3d2e09d5d4d8f818f
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2022-10-30 21:31:51 +0530

    Added solution - LeetHub

---
+++ Chicks in a Zoo - GFG/chicks-in-a-zoo.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+long long int dp[36];
+	long long int NoOfChicks(int n){
+	    // Code here
+	    dp[0] = 0;
+	    dp[1] = 1;
+	    
+        for (int i = 2; i <= 6; i++) {
+            dp[i] = dp[i - 1] * 3;
+        }
+        
+	    dp[7] = 3*dp[6] - 3*dp[1]; //There is no parent 
+	    
+	    //From day 8 there will be a parent which will die on the previous day
+	    //and it's two child will die that day (so overall 2 out of 3 chicks will
+	    //die on (n-6)th day . parent already have died only two kids are alive)
+
+	    for(int i=8;i<=n;i++) dp[i] = (dp[i - 1] - (2 * dp[i - 6] / 3)) * 3;
+	    
+	    return dp[n];
+	   
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int N;
+		cin >> N;
+		Solution obj;
+		long long int ans = obj.NoOfChicks(N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
