545435bf5242d384e90652789c08fd936b57f940
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2023-01-24 20:00:21 +0530

    Added solution - LeetHub

---
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+ 
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+ 
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        // Else the element can only be present
+        // in right subarray
+        return binarySearch(arr, mid + 1, r, x);
+    }
+ 
+    // We reach here when element is not
+    // present in array
+    return -1;
+}
+ 
+	void convert(int arr[], int n) {
+	    // code here
+	    
+	    int*box = (int*)malloc(n*sizeof(int));
+	    
+	    int i;
+	    for(i=0;i<n;i++)
+	    {
+	        box[i] = arr[i];
+	    }
+	    
+	    sort(box, box+n);
+	    int x;
+	    int loc;
+	    for(i=0;i<n;i++)
+	    {
+	       x = arr[i];
+	       loc = binarySearch(box,0,n-1,x);
+	       arr[i] = loc;
+	    }
+	}
+
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.convert(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
