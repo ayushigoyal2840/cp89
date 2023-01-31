2c76eac0fff0ad8f3248f6dac89da70ffee39323
Ayushi goel <61381126+ayushigoyal2840@users.noreply.github.com>
2023-01-31 10:06:47 +0530

    Added solution - LeetHub

---
+++ Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+
+    int minRepeats(string A, string B) {
+
+       int c = 1;
+
+       string s = A;
+
+       while(A.length() <B.length() )
+
+       {
+
+           A = A + s;
+
+           c++;
+
+       }
+
+       
+
+       if(A.find(B) != -1)
+
+        {
+
+            return c;
+
+        }
+
+         A = A +s;
+
+         c++;
+
+         if(A.find(B) != -1)
+
+            return c;
+
+        return -1;
+
+     
+
+        
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ",&t);
+    while (t--) {
+        string A,B;
+        getline(cin,A);
+        getline(cin,B);
+
+        Solution ob;
+        cout << ob.minRepeats(A,B) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
